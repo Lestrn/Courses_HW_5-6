bee82a815ed2291f28d46f2f89ef6154f4d58a5a
Lestrn <ilestrn@gmail.com>
2023-05-05 11:22:28 +0300

    Start of first part

---
+++ Courses_HW_5-6_Part_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Courses_HW_5_6_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
